42d67454f53cab6fe1510281a078681ecdc98096
gcoulby <gcoulby@googlemail.com>
2023-06-16 14:16:00 +0100

    fixing static paths

--- components/project-item.tsx
+++ components/project-item.tsx
@@ -13,7 +13,7 @@ function ProjectItem({ post, setTag }: { post: IPost; setTag: React.Dispatch<Rea
             <div className="crop">
               <Link href={link} className="">
                 <img
-                  src={`/project-images/${post.slug}/feature.png`}
+                  src={`/next-portfolio/project-images/${post.slug}/feature.png`}
                   className="cropped-image"
                   alt="feature for the project"
                   width={512}
