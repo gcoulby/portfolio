f85c8f140d8311a2c9e2f5d3c6f4632e1c8ecbd5
gcoulby <gcoulby@googlemail.com>
2023-06-16 14:35:23 +0100

    fixing static paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Graham Coulby",
   description: "Home page for Graham Coulby",
-  favicon: "/img/favicon.ico",
   metadataBase: "http://grahamcoulby.co.uk/",
   openGraph: {
     type: "website",
@@ -16,27 +15,17 @@ export const metadata = {
     url: "http://grahamcoulby.co.uk/",
     title: "Graham Coulby",
     description: "Graham Coulby is a post-doc computer scientist who is interested in reserarch, IoT, embedded systems and full-stack development.",
-    image: "/img/apple-touch-icon.png",
+    image: "/next-portfolio/img/apple-touch-icon.png",
     site_name: "Graham Coulby",
     imageWidth: 785,
     imageHeight: 411,
-  },
-  icons: {
-    icon: "/img/favicon-32x32.png",
-    shortcut: "/img/favicon.ico",
-    apple: "/img/apple-touch-icon.png",
-    appleTouch: "/img/apple-touch-icon.png",
-    android: "/img/android-chrome-192x192.png",
-    ms: "/img/mstile-150x150.png",
-    safari: "/img/safari-pinned-tab.svg",
   },
   viewport: {
     width: "device-width",
     initialScale: 0.85,
   },
 
-  manifest: "/img/site.webmanifest",
-  icon: "/img/favicon-32x32.png",
+  manifest: "/next-portfolio/img/site.webmanifest",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
