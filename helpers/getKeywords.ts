b695fe92cdf7d1c02b449af2483e486c32843150
Graham <gcoulby@googlemail.com>
2024-07-19 21:01:34 +0100

    changed tag order in sidebar to popular first

--- helpers/getKeywords.ts
+++ helpers/getKeywords.ts
@@ -1,14 +1,17 @@
-import { IPost } from "./interfaces";
+import { IPost } from './interfaces'
 
 const getKeywords = (posts: IPost[]): string[] => {
-  const keywords: string[] = [];
+  const keywordMap: Map<string, number> = new Map()
+
   posts.forEach((post) => {
     post.keywords.forEach((keyword) => {
-      if (!keywords.includes(keyword)) {
-        keywords.push(keyword);
-      }
-    });
-  });
-  return keywords.sort((a, b) => a.localeCompare(b));
-};
-export default getKeywords;
+      keywordMap.set(keyword, (keywordMap.get(keyword) || 0) + 1)
+    })
+  })
+
+  return Array.from(keywordMap.entries())
+    .sort((a, b) => b[1] - a[1])
+    .map((entry) => entry[0])
+}
+
+export default getKeywords
