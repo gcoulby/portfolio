85e311b39910ebcebbbbc45036e34cf561480f88
Graham <gcoulby@googlemail.com>
2024-09-02 12:26:24 +0100

    added note to portfolio

--- components/portfolio-list.tsx
+++ components/portfolio-list.tsx
@@ -1,20 +1,20 @@
-"use client";
-import ProjectsNav from "./projectNav";
-import { useEffect, useState } from "react";
-import logo from "@/images/logo.png";
-import { IPost } from "@/helpers/interfaces";
-import ProjectItem from "./project-item";
-import { FaAngleLeft, FaEye, FaHome, FaTag } from "react-icons/fa";
-import Link from "next/link";
+'use client'
+import ProjectsNav from './projectNav'
+import { useEffect, useState } from 'react'
+import logo from '@/images/logo.png'
+import { IPost } from '@/helpers/interfaces'
+import ProjectItem from './project-item'
+import { FaAngleLeft, FaEye, FaHome, FaTag } from 'react-icons/fa'
+import Link from 'next/link'
 
 export default function PortfolioList({ posts, keywords }: { posts: IPost[]; keywords: string[] }) {
-  const [tag, setTag] = useState("all");
-  const [collapse, setCollapse] = useState(true);
-  const [sortedPosts, setSortedPosts] = useState<IPost[]>([]);
+  const [tag, setTag] = useState('all')
+  const [collapse, setCollapse] = useState(true)
+  const [sortedPosts, setSortedPosts] = useState<IPost[]>([])
 
   useEffect(() => {
-    if (sortedPosts.length === 0) setSortedPosts([...posts].sort(() => Math.random() - 0.5));
-  }, [posts, sortedPosts]);
+    if (sortedPosts.length === 0) setSortedPosts([...posts].sort(() => Math.random() - 0.5))
+  }, [posts, sortedPosts])
 
   return (
     <>
@@ -38,7 +38,7 @@ export default function PortfolioList({ posts, keywords }: { posts: IPost[]; key
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className={`navbar-collapse ${collapse && "collapse"}`} id="navbarSupportedContent">
+          <div className={`navbar-collapse ${collapse && 'collapse'}`} id="navbarSupportedContent">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <Link href="/" className="nav-link js-scroll-trigger">
@@ -47,7 +47,7 @@ export default function PortfolioList({ posts, keywords }: { posts: IPost[]; key
               </li>
               <>
                 <li className="nav-item">
-                  <span className="nav-link js-scroll-trigger" onClick={() => setTag("all")}>
+                  <span className="nav-link js-scroll-trigger" onClick={() => setTag('all')}>
                     <FaEye /> Show All
                   </span>
                 </li>
@@ -60,8 +60,8 @@ export default function PortfolioList({ posts, keywords }: { posts: IPost[]; key
                       <span
                         className="nav-link js-scroll-trigger"
                         onClick={() => {
-                          setCollapse(true);
-                          setTag(keyword);
+                          setCollapse(true)
+                          setTag(keyword)
                         }}
                       >
                         <FaTag /> {keyword}
@@ -76,7 +76,17 @@ export default function PortfolioList({ posts, keywords }: { posts: IPost[]; key
 
         <section className="resume-section" id="o">
           <div className="resume-section-content">
-            {tag === "all"
+            <h4>Projects</h4>
+            <div className="subheading mb-3">A mixed selection professional and CPD projects</div>
+            <div className="alert alert-secondary mb-4">
+              <p>
+                This page is a collection of developments I have either done in my professional work, or just for sheer joy of it. Often more of the
+                latter as I am not always able to share my professional work. I have a keen interest in learning new technologies and methodologies,
+                and I am always looking for new things to learn. My approach to learning is to try and apply it to a mini-project, which can be
+                documented and shared here. This allows me to learn by doing, and to have a reference to look back on.
+              </p>
+            </div>
+            {tag === 'all'
               ? sortedPosts.map((post, i) => <ProjectItem key={`post-item-${i}`} post={post} setTag={setTag} />)
               : sortedPosts
                   .filter((post) => post.keywords.includes(tag))
@@ -85,5 +95,5 @@ export default function PortfolioList({ posts, keywords }: { posts: IPost[]; key
         </section>
       </div>
     </>
-  );
+  )
 }
